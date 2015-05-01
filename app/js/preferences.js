de096c65ec4e2093f98fcb5430ec74d322cd5b00
Angus <angus@Angus-MacBook-Pro.local>
2015-05-02 01:13:23 +0800

    Initial commit

---
+++ app/js/preferences.js
@@ -0,0 +1,35 @@
+$(function() {
+  $("#app-version")
+  .html(quark.appVersion)
+  .click(function() {
+    $(this).toggleClass('pink');
+    if (!quark.debug) {
+      quark.debug = true;
+    } else {
+      quark.debug = false;
+    }
+  });
+
+  if (1 == localStorage.getItem('startAtLogin')) {
+    $('#startAtLogin').prop( 'checked', true);
+  } else {
+    $('#startAtLogin').prop( 'checked', false);
+  }
+
+
+  $('#startAtLogin').change(function() {
+    console.log($(this).is(':checked'));
+    if ($(this).is(':checked')) {
+      quark.setLaunchAtLogin(true);
+      localStorage.setItem('startAtLogin', 1);
+    } else {
+      quark.setLaunchAtLogin(false);
+      localStorage.setItem('startAtLogin', 0);
+    }
+  });
+
+  $('#checkUpdate').click(function() {
+    quark.checkUpdate('https://rawgit.com/angusjune/popppular/master/updater/Appcast.xml');
+  });
+
+});
